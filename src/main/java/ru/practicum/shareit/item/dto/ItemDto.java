c6dadc7ef2cbb3484b1de254fa23b33c7a8a0953
Brel Snezhana <brelok@1-pp.ru>
2024-04-16 18:53:38 +0300

    Проект ShareIt. Техническое задание. Часть 1 Работа над замечаниями ревьювера

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,9 +1,7 @@
 package ru.practicum.shareit.item.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ru.practicum.shareit.item.model.Item;
 
 import javax.validation.constraints.NotBlank;
 
