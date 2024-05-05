408427399f2cd78f4ebf9a1c99b1ef7bb7dc8aad
Brel Snezhana <brelok@1-pp.ru>
2024-05-05 12:26:59 +0300

    Проект ShareIt. Техническое задание. Часть 3 UserControllerTest

---
+++ src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -0,0 +1,144 @@
+package ru.practicum.shareit.user;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.utils.TestingUtils;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private UserService service;
+
+    private UserDto userDto;
+
+    @BeforeEach
+    void setUp() {
+        userDto = TestingUtils.createUserDto();
+    }
+
+    @Test
+    @SneakyThrows
+    void createUser() {
+        when(service.createUser(any()))
+                .thenReturn(userDto);
+        mvc.perform(
+                        post("/users")
+                                .content(mapper.writeValueAsString(userDto))
+                                .characterEncoding(StandardCharsets.UTF_8)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(userDto.getId()), Integer.class))
+                .andExpect(jsonPath("$.name", is(userDto.getName())))
+                .andExpect(jsonPath("$.email", is(userDto.getEmail())));
+    }
+
+    @Test
+    @SneakyThrows
+    void createNotValidUser() {
+        userDto.setEmail("not_valid_email");
+
+        mvc.perform(
+                        post("/users")
+                                .content(mapper.writeValueAsString(userDto))
+                                .characterEncoding(StandardCharsets.UTF_8)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @SneakyThrows
+    void updateUser() {
+        when(service.updateUser(eq(1), any()))
+                .thenReturn(userDto);
+        mvc.perform(
+                        patch("/users/1")
+                                .content(mapper.writeValueAsString(userDto))
+                                .characterEncoding(StandardCharsets.UTF_8)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(userDto.getId()), Integer.class))
+                .andExpect(jsonPath("$.name", is(userDto.getName())))
+                .andExpect(jsonPath("$.email", is(userDto.getEmail())));
+    }
+
+    @Test
+    @SneakyThrows
+    void getAll() {
+        when(service.getAllUsers())
+                .thenReturn(List.of(userDto));
+        mvc.perform(
+                        get("/users")
+                                .content(mapper.writeValueAsString(userDto))
+                                .characterEncoding(StandardCharsets.UTF_8)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].id", is(userDto.getId()), Integer.class))
+                .andExpect(jsonPath("$.[0].name", is(userDto.getName())))
+                .andExpect(jsonPath("$.[0].email", is(userDto.getEmail())));
+    }
+
+    @Test
+    @SneakyThrows
+    void getById() {
+        when(service.getUserById(eq(1)))
+                .thenReturn(userDto);
+        mvc.perform(
+                        get("/users/1")
+                                .content(mapper.writeValueAsString(userDto))
+                                .characterEncoding(StandardCharsets.UTF_8)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(userDto.getId()), Integer.class))
+                .andExpect(jsonPath("$.name", is(userDto.getName())))
+                .andExpect(jsonPath("$.email", is(userDto.getEmail())));
+    }
+
+    @Test
+    @SneakyThrows
+    void deleteUser() {
+        doNothing().when(service).deleteUserById(eq(1));
+        mvc.perform(
+                        delete("/users/1")
+                                .characterEncoding(StandardCharsets.UTF_8)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk());
+    }
+}
