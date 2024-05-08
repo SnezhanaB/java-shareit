4cae3abff84672e58c0796085c9bb338e5a077a3
Brel Snezhana <brelok@1-pp.ru>
2024-05-09 01:34:08 +0300

    Проект ShareIt. Техническое задание. Часть 3 UserServiceImplTest

---
+++ src/test/java/ru/practicum/shareit/user/UserServiceImplTest.java
@@ -0,0 +1,125 @@
+package ru.practicum.shareit.user;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.shareit.exception.AlreadyExistsException;
+import ru.practicum.shareit.exception.DataValidationException;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.utils.TestingUtils;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@Transactional
+@SpringBootTest(
+        properties = "db.name=test",
+        webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@AutoConfigureTestDatabase
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+class UserServiceImplTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserServiceImpl service;
+
+    private final ModelMapper mapper = new ModelMapper();
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void createUser() {
+        UserDto userDto = TestingUtils.createUserDto();
+        User user = mapper.map(userDto, User.class);
+        when(userRepository.save(any()))
+                .thenReturn(user);
+
+        UserDto saved = service.createUser(userDto);
+        assertEquals(saved, userDto);
+        verify(userRepository, times(1)).save(any());
+    }
+
+    @Test
+    void updateUser() {
+        UserDto userDto = TestingUtils.createUserDto(2);
+        userDto.setId(1);
+        User user = TestingUtils.createUser(1);
+
+        when(userRepository.getUserById(anyInt()))
+                .thenReturn(user);
+        when(userRepository.findUserByEmail(anyString()))
+                .thenReturn(null);
+        when(userRepository.save(any()))
+                .thenReturn(user);
+
+        UserDto saved = service.updateUser(userDto.getId(), userDto);
+        assertEquals(saved, userDto);
+        verify(userRepository, times(1)).save(any());
+        verify(userRepository, times(1)).findUserByEmail(anyString());
+
+    }
+
+    @Test
+    void updateUse_duplicateEmail() {
+        UserDto userDto = TestingUtils.createUserDto(2);
+        userDto.setId(1);
+        User user = TestingUtils.createUser(1);
+
+        when(userRepository.getUserById(anyInt()))
+                .thenReturn(user);
+        when(userRepository.findUserByEmail(anyString()))
+                .thenReturn(user);
+        when(userRepository.save(any()))
+                .thenReturn(user);
+
+        AlreadyExistsException e = assertThrows(
+                AlreadyExistsException.class,
+                () -> service.updateUser(userDto.getId(), userDto)
+        );
+        assertEquals(e.getMessage(), "Пользователь с email=" + userDto.getEmail() + " уже существует");
+    }
+
+    @Test
+    void updateUse_sameEmail() {
+        UserDto userDto = TestingUtils.createUserDto(1);
+        userDto.setId(1);
+        User user = TestingUtils.createUser(1);
+
+        when(userRepository.getUserById(anyInt()))
+                .thenReturn(user);
+        when(userRepository.findUserByEmail(anyString()))
+                .thenReturn(user);
+        when(userRepository.save(any()))
+                .thenReturn(user);
+
+        UserDto saved = service.updateUser(userDto.getId(), userDto);
+        assertEquals(saved, userDto);
+        verify(userRepository, times(1)).save(any());
+        verify(userRepository, times(0)).findUserByEmail(anyString());
+    }
+
+    @Test
+    void getUserById() {
+    }
+
+    @Test
+    void deleteUserById() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+}
